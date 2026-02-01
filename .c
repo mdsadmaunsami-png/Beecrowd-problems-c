5d8e98535270d72a8ad7d9481975c76c3067cb68
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-02-01 13:05:08 +0600

    Refactor main function for better clarity and types

--- .c
+++ .c
@@ -15,3 +15,34 @@ int main()
    printf("summision =%d\nDifference=%d\nMlutiplication=%d\nDivision=%.2f",sum,diff,mul,division);
    return 0;
 }
+
+or
+
+# include <stdio.h>
+int main() 
+{ 
+    int  num1, num2,sum,diff,mult;
+    printf(" enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    diff = num1 - num2;
+    mult = num1 * num2;
+    printf(" sum = %d\ndiff = %d\nmult = %d\ndiv = %.2f", sum, diff, mult, (float)sum / 2);
+    return 0;
+}
+
+or
+
+# include <stdio.h>
+int main() 
+{ 
+    float  num1, num2,sum,diff,mult,div;
+    printf(" enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+    sum = num1 + num2;
+    diff = num1 - num2;
+    mult = num1 * num2;
+    div = sum/2;
+    printf(" sum = %.2f\ndiff = %.2f\nmult = %.2f\ndiv = %.2f", sum, diff, mult, div);
+    return 0;
+}
